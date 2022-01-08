0cb57e4a13a96cfd912ee03f76d13947f49a3b6c
Rhys Maxwell <58702032+RMaxwellGit@users.noreply.github.com>
2022-01-08 17:22:02 -0500

    Moved the existing work into the repo
    Existing work consists of a controllable car and placeholder field, as well as controllable FOV, screen shake, motion blur and lens distortion

---
+++ Speed Juice/Assets/CameraAnchorBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAnchorBehaviour : MonoBehaviour
+{
+    public Rigidbody rbCam;
+    public Transform tPivot;
+    public float shakeForce;
+    public bool doShake;
+
+    // Update is called once per frame
+    void Update()
+    {
+        tPivot.rotation = transform.rotation; //replace this with camera lerping towards looking at the car
+
+        if (doShake) Shake();
+    }
+
+    public void SetShakeForce(float newSF) {
+        shakeForce = newSF;
+    }
+    public void SetDoShake(bool newDS) {
+        doShake = newDS;
+    }
+
+    public void Shake() {
+        Shake(1);
+    }
+
+    public void Shake(float speedMultiplier) {
+        Vector3 dir = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-0.5f, 0.5f));
+        rbCam.AddForce(dir * shakeForce);
+    }
+}
