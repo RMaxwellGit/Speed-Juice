88fceb4b12bdedcc4c4b66ab907c199da43866e5
Rhys Maxwell <58702032+RMaxwellGit@users.noreply.github.com>
2022-01-16 13:42:55 -0500

    Changed FOV to dynamically increase with player speed

---
+++ Speed Juice/Assets/DynamicFOV.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicFOV : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float baseFOV, maxFOV;
+
+    public float minFOVExpansionSpeed, maxFOVExpansionSpeed;
+
+    private float curSpeed;
+
+    private float CalculateFOV() {
+        curSpeed = rb.velocity.magnitude;
+        Debug.Log(curSpeed);
+        float newFOV;
+
+        if (curSpeed >= minFOVExpansionSpeed) {
+            // newFOV = (15f * (Mathf.Cos(((6.28f * curSpeed) / 61.3f) + 3.3f))) + 75f;
+            float fovFactor = (curSpeed - minFOVExpansionSpeed) / (maxFOVExpansionSpeed - minFOVExpansionSpeed);
+            newFOV = Mathf.Lerp(baseFOV, maxFOV, fovFactor);
+        } else {
+            newFOV = baseFOV;
+        }
+        return newFOV;
+    }
+
+    void Update()
+    {
+        Camera.main.fieldOfView = CalculateFOV();
+    }
+}
